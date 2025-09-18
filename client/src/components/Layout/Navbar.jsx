f80a4bb5c961738d069fad3fb194956a3b53c7a4
nilubhai93 <niladrimaji9382@gmail.com>
2025-09-19 01:59:42 +0530

    todo list

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaPowerOff } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import toast from "react-hot-toast";
+
+
 
 const Navbar = () => {
+    const [username, setUsername] = useState("")
+    useEffect(() => {
+        const userData = JSON.parse(localStorage.getItem("todoapp"))
+        setUsername(userData && userData.user.username)
+    }, []);
+
+
+
+    // log out
+    const navigate = useNavigate();
+    const logoutHandler = () => {
+        localStorage.removeItem("todoapp");
+        toast.success("logout sucessfully ✅");
+        navigate("/login");
+    }
+
+
+
     return (
         <>
             <div className="flex flex-1  sm:items-stretch sm:justify-start">
@@ -10,7 +31,7 @@ const Navbar = () => {
                 <div className="flex justify-between w-full  ">
                     <div className="flex justify-between w-full space-x-4 px-1 bg-gray-200">
                         <h2>
-                            <a href="#" className="rounded-md  text-3xl font-bold text-black">Welcome User</a>
+                            <a href="#" className="rounded-md  text-3xl font-bold text-black">Welcome {username}</a>
                         </h2>
 
                         <div className='flex justify-end ml-auto'>
@@ -20,8 +41,11 @@ const Navbar = () => {
                             <Link to="/todolist" href="#" className="
                             nav-link rounded-md px-3 py-2 text-sm font-medium text-black"> My Todo </Link>
 
-                            <Link href="#" className=" nav-link text-red-400 text-3xl rounded-md px-3 py-2  font-medium" title="logout"><FaPowerOff /></Link>
-
+                            <button className=" nav-link text-red-400 text-3xl rounded-md px-3 py-2  font-medium"
+                                title="logout"
+                                onClick={logoutHandler}>
+                                <FaPowerOff />
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -29,6 +53,7 @@ const Navbar = () => {
         </>
     )
 }
+
 
 
 export default Navbar
